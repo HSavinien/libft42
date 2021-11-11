fa9e1d4f4b426139053266b1bcb6b0abdb8c1420
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-11 14:49:09 +0100

    corrected forbidden fct usage in ft_putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,17 +6,40 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 15:48:54 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/09 16:13:17 by tmongell         ###   ########.fr       */
+/*   Updated: 2021/11/11 14:48:22 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 
+static int	ft_handlenegative(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		write(fd, "-2", 2);
+		return (n *-10);
+	}
+	else
+	{
+		write(fd, "-", 1);
+		return (-n);
+	}
+}
+
+static char ft_get_digit(int n)
+{
+	return ((n % 10) + '0');
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*nb_str;
+	char	digit;
 
-	nb_str = ft_itoa(n);
-	write(fd, nb_str, ft_strlen(nb_str));
+	if (n < 0)
+		n = ft_handlenegative(n, fd);
+	if (n >= 10)
+		ft_putnbr_fd(n / 10 , fd);
+	digit = ft_get_digit(n);
+	write(fd, &digit, 1);
 }
