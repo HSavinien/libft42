6f4d797ee39fc64f024e7e7d944e1db23592a1cb
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-12 15:49:40 +0100

    added test info to function prototype : functions that fail tests as marked as KO

--- libft.h
+++ libft.h
@@ -6,13 +6,19 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 13:38:12 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/09 16:14:20 by tmongell         ###   ########.fr       */
+/*   Updated: 2021/11/12 15:11:00 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <string.h>
+//structs :
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 // function from part 1 :
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -23,25 +29,25 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t n);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+void	*ft_memmove(void *dst, const void *src, size_t n);//tri,njj==============KO
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);//tri=============KO
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);//tri,njj=========KO
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strchr(const char *s, int c);//tri,njj===============================KO
+char	*ft_strrchr(const char *s, int c);//tri==================================KO
+int		ft_strncmp(const char *s1, const char *s2, size_t n);//tri,njj===========KO
 void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);//tri,njj============KO
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 //function from part 2 :
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2);//tri,njj====================KO
 char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);//WARNING : function not finished=====
+char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
@@ -50,7 +56,7 @@ void	ft_putstr_fd(char *c, int fd);
 void	ft_putendl_fd(char *c, int fd);
 void	ft_putnbr_fd(int n, int fd);
 //functions from bonus
-
+t_list	*ft_lstnew(void *content);
 //additional functions (not mandatory, but still used)
 char	*ft_strndup(const char *s1, size_t n);
 #endif
