8c15e04e700aadc54cdca365d7a7d15d8c67b75e
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-15 19:29:24 +0100

    corrected strjoin so that empty first string don't break stuff. still don't know why it did tho

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,12 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 16:30:00 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/15 19:01:46 by tmongell         ###   ########.fr       */
+/*   Updated: 2021/11/15 19:27:53 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
-#include <stdio.h>//============================================================DEBUG
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -21,9 +20,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	len = ft_strlen(s1) + ft_strlen(s2);
 	new_str = malloc(len + 1);
-//	printf("debug : malloc of size %lu\n", len + 1);//==========================DEBUG
 	if (new_str == NULL)
 		return (NULL);
+	ft_bzero(new_str, len + 1);
 	ft_strlcat (new_str, s1, len + 1);
 	ft_strlcat (new_str, s2, len + 1);
 	new_str[len] = '\0';
