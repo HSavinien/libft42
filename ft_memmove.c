40c614f9d940f6ee294309da63f24c74872f69d3
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-15 16:52:33 +0100

    corrected ft_memmove and corresponding test

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,20 +6,22 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 18:36:52 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/02 12:46:49 by tmongell         ###   ########.fr       */
+/*   Updated: 2021/11/15 16:49:44 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>//============================================================DEBUG
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t		i;
 
-	if (!src)
+	if (!src || !dst)
 		return (NULL);
 	if (dst < src)
 	{
+		printf("debug : dst < src\n");//========================================DEBUG
 		i = 0;
 		while (i < len)
 		{
@@ -29,12 +31,18 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	}
 	else
 	{
-		i = len;
+		printf("debug : dst > src\n");//========================================DEBUG
+		if (ft_strlen(src) < len)
+			i = ft_strlen(src);
+		else
+			i = len;
 		while (i > 0)
 		{
 			*((char *)dst + i) = *((char *)src + i);
+			printf("debug : added %c to dst\n", *((char *)src + i));//==========DEBUG
 			i --;
 		}
+		*((char *)dst + i) = *((char *)src + i);
 	}
 	return (dst);
 }
