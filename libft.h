4705dc68a83fb261946845d832e032b8646b68a0
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-15 17:41:02 +0100

    updated libft.h comment about failing tester, no big deal

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 13:38:12 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/12 20:51:14 by tmongell         ###   ########.fr       */
+/*   Updated: 2021/11/15 17:39:23 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,13 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t n);//tri,njj==============KO
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);//tri=============KO
+void	*ft_memmove(void *dst, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);//tri,njj===============================KO
-char	*ft_strrchr(const char *s, int c);//tri==================================KO
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);//tri,njj===========KO
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);//tri,njj============KO
