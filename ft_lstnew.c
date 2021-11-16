7db15f55feea5a9f2dc0db833fdd63f2821d6cb5
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-16 19:23:14 +0100

    completed ft_lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/10 13:33:03 by tmongell          #+#    #+#             */
+/*   Updated: 2021/11/16 19:21:03 by tmongell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_lst;
+	
+	new_lst = malloc(sizeof(t_list));
+	new_lst->content = ft_strdup(content);
+	new_lst->next = NULL;
+	return (new_lst);
+}
+
