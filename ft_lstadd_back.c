8f7d37bbf06f465207a8c23d440381fc743c73e8
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-17 18:04:20 +0100

    added ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,20 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*   lstadd_back.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/17 17:12:11 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/17 17:39:52 by tmongell         ###   ########.fr       */
+/*   Created: 2021/11/17 17:40:22 by tmongell          #+#    #+#             */
+/*   Updated: 2021/11/17 18:02:56 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstlast(t_list *lst)
-{//not tested yet : tester requier an other fct that don't exist yet============TEST
-	while (lst->next)
-		lst = lst->next;
-	return (lst);
+void	ft_lstadd_back(t_list	**alst, t_list *new)
+{
+	alst = new;
 }
