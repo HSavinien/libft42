ff35baff729c2c3f1a2d3a02c82e0b1e6d9b1bb8
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-17 17:00:18 +0100

    norminette correction

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 13:46:40 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/17 16:57:57 by tmongell         ###   ########.fr       */
+/*   Updated: 2021/11/17 16:59:43 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 void	ft_lstadd_front(t_list **alst, t_list *new)
 {
-	//change new.next so it point toward *alst
 	new->next = *alst;
 	*alst = new;
 }
