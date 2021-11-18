1b722c4a18bd407ffb580831a1e4450e85646083
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-18 16:29:39 +0100

    ft_lstadd_back mow compile

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,20 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lstadd_back.c                                      :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/17 17:40:22 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/18 15:23:02 by tmongell         ###   ########.fr       */
+/*   Created: 2021/11/18 15:43:35 by tmongell          #+#    #+#             */
+/*   Updated: 2021/11/18 16:27:56 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list	**alst, t_list *new)
+void	ft_lstadd_back(t_list **alst, t_list *new)
 {
-	while (alst->next)
-		alst = alst->next;
-	alst->next = new;
+	while ((*alst)->next)
+	{
+		(*alst) = (*alst)->next;
+	}
+	(*alst)->next = new;
 }
