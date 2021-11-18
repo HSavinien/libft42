20c9c271dc5c638dbba77457b888283c8faf0c9a
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-18 17:59:56 +0100

    protected lstlast against null pointers

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,14 +6,16 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 17:12:11 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/17 17:39:52 by tmongell         ###   ########.fr       */
+/*   Updated: 2021/11/18 17:57:25 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 t_list	*ft_lstlast(t_list *lst)
-{//not tested yet : tester requier an other fct that don't exist yet============TEST
+{
+	if (!lst)
+		return (lst);
 	while (lst->next)
 		lst = lst->next;
 	return (lst);
