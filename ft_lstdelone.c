cb568acea0ad3739038a428a0775638cbb511d50
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-18 18:19:05 +0100

    lstdelone debug

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 18:06:24 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/18 18:17:13 by tmongell         ###   ########.fr       */
+/*   Updated: 2021/11/18 18:18:28 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	t_list	next;
 
 	next = *(lst)->next;
-	if (*lst)
+	if (lst)
 	{
 		del(lst->content);
 		free(lst);
