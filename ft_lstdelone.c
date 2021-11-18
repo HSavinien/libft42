6ad29436203ed44201ee342a87af59a2433fa5a1
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-18 18:17:42 +0100

    protected ft_lstdelone against null pointers

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 18:06:24 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/18 18:13:08 by tmongell         ###   ########.fr       */
+/*   Updated: 2021/11/18 18:17:13 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	t_list	next;
 
 	next = *(lst)->next;
-	del(lst->content);
-	free(lst);
+	if (*lst)
+	{
+		del(lst->content);
+		free(lst);
+	}
 	*lst = next;
 }
