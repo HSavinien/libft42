d570182227ccfdfe0d18d7bf0764f9617b8668f8
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-18 15:23:53 +0100

    reworked ft_lstad_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 17:40:22 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/17 18:06:57 by tmongell         ###   ########.fr       */
+/*   Updated: 2021/11/18 15:23:02 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	ft_lstadd_back(t_list	**alst, t_list *new)
 {
-	*alst = new;
+	while (alst->next)
+		alst = alst->next;
+	alst->next = new;
 }
