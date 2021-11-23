33884d1b987959ca84a50ecdb9ac69dfc1780841
Theo Mongellaz <theo.mongellaz@gmail.com>
2021-11-23 18:22:19 +0100

    protected strlcpy against void dst

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 16:31:10 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/23 16:10:24 by tmongell         ###   ########.fr       */
+/*   Updated: 2021/11/23 18:21:23 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	int	i;
 
+	if (!dst)
+		return (ft_strlen(src));
 	i = 0;
 	while (i < (int)dstsize && src)
 	{
